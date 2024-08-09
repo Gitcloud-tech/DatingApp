1ed02afaf25f0caf9805cf4707020d53b1b19b6f
LAPTOP-Q901OA94\Admin <gopald099504@gmail.com>
2024-08-09 11:22:56 +0530

    Changes for register and login component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,26 +1,34 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
+import { FormsModule } from '@angular/forms';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, NavComponent, FormsModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   title = 'Dating app';
-  users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private accountService: AccountService) {}
   ngOnInit(): void {
-    this.http.get('https://localhost:7111/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
+    this.setCurrentUser();
+  }
+
+
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 
 
